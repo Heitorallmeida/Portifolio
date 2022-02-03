8eea143a90bfb02dedd1a392f792b781f9700a5f
Heitor Almeida <hnma@ic.ufal.br>
2022-02-03 13:33:25 -0300

    alinhando itens

--- src/components/Courses/styles.tsx
+++ src/components/Courses/styles.tsx
@@ -10,11 +10,21 @@ export const certificados = styled(Typography)`
 export const container = styled.div`
     padding: 2rem;
     display:flex;
+    @media (max-width: 600px)
+    {
+        flex-direction: column;
+        align-items: center;
+    }
 `;
 
 export const coursesContainer = styled.div`
     min-width: 50vw;
     display: flex;
+    @media (max-width: 600px)
+    {
+        flex-direction: column;
+        align-items: center;
+    }
     overflow-x: scroll;
     scrollbar-color: #051932 white;
 `;
