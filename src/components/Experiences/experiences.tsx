f596996ed03ad5f78c15926f5756b8c6b64d3c73
Heitorallmeida <hnma@ic.ufal.br>
2022-08-06 14:42:50 -0300

    adjusting responsivity

--- src/components/Experiences/experiences.tsx
+++ src/components/Experiences/experiences.tsx
@@ -1,45 +1,42 @@
-import ExperienceItem from "./experienceItem";
+import ExperienceItem from "./components/experienceItem";
 import propep from "../../assets/propep.png";
 import ufal from "../../assets/ufal.png";
 import nti from "../../assets/nti.jpg";
 import accio from "../../assets/accio.png";
 import edge from "../../assets/edge.png";
 import vitalis from "../../assets/vitalis.png";
-import { makeStyles, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { experiencesWrapper, row } from "./styles";
 
-const useStyles = makeStyles({
-    row: {
-      display: 'flex',
-      flexDirection: 'row',
-      '@media (max-width: 600px)': {
-        flexDirection: 'column',
-      },
-    },
-    experiencesWrapper: {
-        background: "linear-gradient(to right, #ece9e6, #fff)"
-    }
-});
-
-function Experiences(){
-    
-    const classes = useStyles();
-    
-    return(
-        <div className = {classes.experiencesWrapper}>
-            <Typography variant="h3" component="h2" style={{'fontWeight': 'bold', 'textAlign':'center', 'marginTop': '5rem'}}>Experiências</Typography>
-            <Typography variant="h4" component="h2" style={{'fontWeight': 'bold', 'textAlign':'center', 'marginTop': '1rem'}}>Iniciou Engenharia de Computação</Typography>
-            <div className = {classes.row}>
-                <ExperienceItem nome = "Bolsista" imagem = {propep}></ExperienceItem>
-                <ExperienceItem nome = "" imagem = {ufal}></ExperienceItem>
-                <ExperienceItem nome = "Dev Full Stack" imagem = {nti}></ExperienceItem>
-            </div>
-            <div className = {classes.row}>
-                <ExperienceItem nome = "Dev Full Stack" imagem = {accio}></ExperienceItem>
-                <ExperienceItem nome = "Dev Front End" imagem = {edge}></ExperienceItem>
-                <ExperienceItem nome = "Dev Front End" imagem = {vitalis}></ExperienceItem>
-            </div>
-        </div>
-    );
+function Experiences() {
+  return (
+    <div className={experiencesWrapper}>
+      <Typography
+        variant="h3"
+        component="h2"
+        style={{ fontWeight: "bold", textAlign: "center", marginTop: "6rem" }}
+      >
+        Experiências
+      </Typography>
+      <Typography
+        variant="h4"
+        component="h2"
+        style={{ fontWeight: "bold", textAlign: "center", marginTop: "1rem" }}
+      >
+        Iniciou Engenharia de Computação
+      </Typography>
+      <div className={row}>
+        <ExperienceItem nome="Bolsista" imagem={propep}></ExperienceItem>
+        <ExperienceItem nome="" imagem={ufal}></ExperienceItem>
+        <ExperienceItem nome="Dev Full Stack" imagem={nti}></ExperienceItem>
+      </div>
+      <div className={row}>
+        <ExperienceItem nome="Dev Full Stack" imagem={accio}></ExperienceItem>
+        <ExperienceItem nome="Dev Front End" imagem={edge}></ExperienceItem>
+        <ExperienceItem nome="Dev Front End" imagem={vitalis}></ExperienceItem>
+      </div>
+    </div>
+  );
 }
 
 export default Experiences;
---
+++ src/components/Experiences/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const row = styled.div`
+  display: "flex";
+  flex-direction: "row";
+  @media (max-width: 600px) {
+    flex-direction: "column";
+  }
+`;
+
+export const experiencesWrapper = styled.div`
+  background: "linear-gradient(to right, #ece9e6, #fff)";
+`;
